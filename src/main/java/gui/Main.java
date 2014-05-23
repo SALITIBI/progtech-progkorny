75b4ee756a58889bc1548ed341b4d1a885deab70
Salagvárdi Tibor <salagvardit@msn.com>
2014-05-23 22:42:09 +0200

    Added Javadoc comments to Main class.

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -5,7 +5,6 @@ import io.FavoritesStorage;
 import io.FavoritesStorageImpl;
 import io.MovieStorage;
 import io.MovieStorageImpl;
-
 import java.awt.EventQueue;
 
 import org.slf4j.Logger;
@@ -13,9 +12,22 @@ import org.slf4j.LoggerFactory;
 
 import controller.GuiEventListener;
 import controller.Controller;
-
+/**
+ * 
+ * Class containing the main-program.
+ * 
+ * @author Tibor Salagvárdi
+ *
+ */
 public class Main {
+	/**
+	 * Logger for logging.
+	 */
 	private static Logger logger = LoggerFactory.getLogger(Main.class);
+	/**
+	 * Method for initializing a {@code Controller} object and the IO sources for it. 
+	 * @return the initialized {@code Controller} object
+	 */
 	public static Controller initializeModel()
 	{
 		Controller state;
@@ -24,6 +36,10 @@ public class Main {
 		state = new Controller(movs,favs);
 		return state;
 	}
+	/**
+	 * The main-program itself. Used for initializing the software.
+	 * @param args command-line arguments, which has no effect on the software at all
+	 */
 	public static void main(String[] args) {
 		final Controller state=initializeModel();
 		EventQueue.invokeLater(new Runnable() {
